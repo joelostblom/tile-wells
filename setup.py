d162f214a43ba27f5cac7e8a37a616bdce291812
joelostblom <joel.ostblom@gmail.com>
2017-09-21 09:26:49 -0400

    Make into python package

---
+++ setup.py
@@ -0,0 +1,20 @@
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='tile-wells',
+    url='git@github.com:joelostblom/tile-wells.git',
+    author='Joel Ostblom',
+    author_email='joel.ostblom@gmail.com',
+    # Needed to actually package something
+    packages=['tile-wells'],
+    # Needed for dependencies
+    install_requires=['matplotlib', 'joblib'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='''Place the well images of tiled fields in a layout similar to a multiwell
+plate. This makes it easy to get an overview of the different conditions in
+the plate.''',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
